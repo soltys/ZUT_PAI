beb4be6ff21b9be530c84b0b7ee5e4057e30149b
Paweł Sołtysiak <soltys@uznam.org>
2013-11-06 13:33:10 +0100

    added documents for lab2

--- lab2/dist.bat
+++ lab2/dist.bat
@@ -12,4 +12,22 @@ set PATH=%PATH%;%sevenZipPath%
 
 del %ZIPNAME%
 
-7z a -mx9 -tzip %ZIPNAME% projekt-01-01 zad7
+cd mysql_document
+call make_pdf
+cd ..
+copy mysql_document\mysql.pdf mysql.pdf
+
+cd mysql_document
+call make_pdf
+cd ..
+copy mysql_document\mysql.pdf mysql.pdf
+
+cd mod_rewrite_document
+call make_pdf
+cd ..
+copy mod_rewrite_document\mod_rewrite.pdf mod_rewrite.pdf
+
+7z a -mx9 -tzip %ZIPNAME% projekt-01-01 zad7 mysql.pdf mod_rewrite.pdf
+
+del mysql.pdf
+del mod_rewrite.pdf
